2860dc6893789d7013d05b502672378397d4c3c3
SK_Wang <skwang_work@foxmail.com>
2015-11-10 10:02:39 +0800

    init

---
+++ DemoBranch/DemoBranch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoBranch
+//
+//  Created by Apple on 15/11/10.
+//  Copyright © 2015年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoBranch/DemoBranch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DemoBranch
+//
+//  Created by Apple on 15/11/10.
+//  Copyright © 2015年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
